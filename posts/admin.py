a36e7e930d0a410ca71e9e429d38abb353da33f9
Didier Zúñiga <didier35@hotmail.com>
2018-08-05 22:59:44 -0500

    [add, update] Creating app/model post and register in Admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+# Models
+from posts.models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('pk', 'title')
+	list_display_links = ('pk', 'title')
+	search_fields = ('pk', 'title')
+	list_filter = ('created', 'modified')
